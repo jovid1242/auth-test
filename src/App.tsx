9c141ba013a3584ad5ca59198a9fe2c26feaa415
jovid1242 <jovid1242@gmail.com>
2022-08-15 19:25:26 +0500

    auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from "react"
 
+// antd
+import { Layout } from "antd"
+
 // components
 import AppRouter from "components/AppRouter"
+import Navbar from "components/navbar"
+
+// styles
+import "./App.css"
 
 function App() {
     return (
-        <div>
-            <AppRouter />
-        </div>
+        <Layout>
+            <Navbar />
+            <Layout.Content>
+                <AppRouter />
+            </Layout.Content>
+        </Layout>
     )
 }
 
---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,62 @@
+import React, { FC } from "react"
+import { useLocation, useNavigate } from "react-router-dom"
+
+// antd
+import { Col, Layout, Menu, Row, Space } from "antd"
+import { RouteName } from "router"
+
+// hooks
+import { useTypedSelector } from "hooks/useTypedSelector"
+
+const Navbar: FC = () => {
+    const { isAuth } = useTypedSelector((state) => state.auth)
+
+    let location = useLocation()
+    const navigate = useNavigate()
+
+    return (
+        <Layout.Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+            <Row>
+                <Col span={22}>
+                    <div className="logo">
+                        <h2 style={{ color: "#fff" }}>Logo</h2>
+                    </div>
+                </Col>
+                <Col span={2}>
+                    {isAuth ? (
+                        <Space>
+                            <div className="" style={{ color: "#fff" }}>
+                                Jovid
+                            </div>
+                            <Menu theme="dark" mode="horizontal">
+                                <Menu.Item
+                                    onClick={() => console.log("logout")}
+                                    key="1"
+                                >
+                                    Выйти
+                                </Menu.Item>
+                            </Menu>
+                        </Space>
+                    ) : (
+                        <>
+                            <Menu
+                                theme="dark"
+                                mode="horizontal"
+                                selectable={false}
+                            >
+                                <Menu.Item
+                                    onClick={() => navigate(RouteName.LOGIN)}
+                                    key="1"
+                                >
+                                    LOGIN
+                                </Menu.Item>
+                            </Menu>
+                        </>
+                    )}
+                </Col>
+            </Row>
+        </Layout.Header>
+    )
+}
+
+export default Navbar
